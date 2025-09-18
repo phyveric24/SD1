07b0ec275da41be455a639d2b5aad569bd0e8271
BING BONG <simonphyveric@gmail.com>
2025-09-18 12:45:41 +0800

    Your commit message

---
+++ navigator/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App(){
+  return (<></>)
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
